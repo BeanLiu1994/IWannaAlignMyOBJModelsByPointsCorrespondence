be71f2e5571ac624d6ee7c79fc2c8fe882c48b06
BeanLiu <njueebeanliu@hotmail.com>
2016-11-16 18:59:04 +0800

    才发现有三个奇怪的文件。这个与仓库内容无关。

--- test.m
+++
@@ -1,8 +0,0 @@
-
-[Tri,Pts]=obj_read('test1.obj');
-[Tri2,Beq]=obj_read('test2.obj');
-Aeq=[1;5;23;54;112;167;268;368;279;489;478];
-Beq=Beq(Aeq,:);
-Beq=Beq+rand(size(Beq))/100;
-
-obj_write('recover.obj',Tri,t)
